aea9e9d2ed57472e71fa5bb54a752a92cbdc25b8
Sonshi04 <74559245+Sonshi04@users.noreply.github.com>
2023-02-23 11:41:19 +0900

    Update img_import.m

--- img_import.m
+++ img_import.m
@@ -12,17 +12,17 @@ for i=1:size(list,1)
   fname=strcat(OUTDIR,'/',num2str(i,'%04d'),'.jpg');
   websave(fname,list{i});
 end
-list=textread('pos.txt','%s');
-OUTDIR='pos';
-mkdir(OUTDIR);
-for i=1:size(list,1)
-  fname=strcat(OUTDIR,'/',num2str(i,'%04d'),'.jpg');
-  websave(fname,list{i});
-end
-list=textread('pos_interesting.txt','%s');
-OUTDIR='pos_interesting';
-mkdir(OUTDIR);
-for i=1:size(list,1)
-  fname=strcat(OUTDIR,'/',num2str(i,'%04d'),'.jpg');
-  websave(fname,list{i});
-end
+% list=textread('pos.txt','%s');
+% OUTDIR='pos';
+% mkdir(OUTDIR);
+% for i=1:size(list,1)
+%   fname=strcat(OUTDIR,'/',num2str(i,'%04d'),'.jpg');
+%   websave(fname,list{i});
+% end
+% list=textread('pos_interesting.txt','%s');
+% OUTDIR='pos_interesting';
+% mkdir(OUTDIR);
+% for i=1:size(list,1)
+%   fname=strcat(OUTDIR,'/',num2str(i,'%04d'),'.jpg');
+%   websave(fname,list{i});
+% end
